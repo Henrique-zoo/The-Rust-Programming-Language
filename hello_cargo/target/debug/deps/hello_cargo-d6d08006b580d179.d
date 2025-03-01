30e86efa9e1e861974bddc3f5f90041136cda58c
Henrique-zoo <morcelleshenrique@gmail.com>
2025-03-01 13:07:54 -0300

    Remove hello_cargo/target from Git tracking

--- hello_cargo/target/debug/deps/hello_cargo-d6d08006b580d179.d
+++
@@ -1,5 +0,0 @@
-C:\Users\henri\OneDrive\Documentos\Projetos\The Rust Programming Language\hello_cargo\target\debug\deps\libhello_cargo-d6d08006b580d179.rmeta: src\main.rs
-
-C:\Users\henri\OneDrive\Documentos\Projetos\The Rust Programming Language\hello_cargo\target\debug\deps\hello_cargo-d6d08006b580d179.d: src\main.rs
-
-src\main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-C:\Users\henri\OneDrive\Documentos\Projetos\The\ Rust\ Programming\ Language\hello_cargo\target\debug\hello_cargo.exe: C:\Users\henri\OneDrive\Documentos\Projetos\The\ Rust\ Programming\ Language\hello_cargo\src\main.rs
